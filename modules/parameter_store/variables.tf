a84b02b14ad817f83f819e129cfeaf64da8f166e
md-jakir <mdavir814@gmail.com>
2025-01-28 14:42:17 +0600

    fix variable issue

--- modules/parameter_store/variables.tf
+++ modules/parameter_store/variables.tf
@@ -15,47 +15,56 @@ locals {
     nextauth_secret = {
       description = "Secret Value for nextauth"
       type        = "String"
-      value       = getenv("NEXTAUTH_SECRET", "")
+      #value       = getenv("NEXTAUTH_SECRET", "")
+      default = ""
     },
     db_url = {
       description = "Database URL for the application"
       type        = "SecureString"
-      value       = getenv("DB_URL", "")
+      #value       = getenv("DB_URL", "")
+      default = ""
     },
     jwt_secret = {
       description = "JWT Secret for signing"
       type        = "String"
-      value       = getenv("JWT_SECRET", "")
+      #value       = getenv("JWT_SECRET", "")
+      default = ""
     },
     jwt-secret-for-verify = {
       description = "JWT Secret for verification"
       type        = "String"
-      value       = getenv("JWT_SECRET_FOR_VERIFY", "")
+      #value       = getenv("JWT_SECRET_FOR_VERIFY", "")
+      default = ""
     },
     openai-api-key = {
       description = "OpenAI API Key"
       type        = "String"
-      value       = getenv("OPENAI_API_KEY", "")
+      #value       = getenv("OPENAI_API_KEY", "")
+      default = ""
     },
     sender-password = {
       description = "Sender Password"
       type        = "String"
-      value       = getenv("SENDER_PASSWORD", "")
+      #value       = getenv("SENDER_PASSWORD", "")
+      default = ""
     },
     access-key = {
       description = "access key"
       type        = "String"
-      value       = getenv("key", "")
+      #value       = getenv("key", "")
+      default = ""
     },
     secret-key = {
       description = "secret key"
       type        = "String"
-      value       = getenv("secret", "")
+      #value       = getenv("secret", "")
+      default = ""
     },
     session-token = {
       description = "Session Token"
       type        = "String"
-      value       = getenv("session_token", "")
+      #value       = getenv("session_token", "")
+      default = ""
     }
   }
 }
